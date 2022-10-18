1559fcdc3038dbbd792328fa30968bc9b2893b12
JRMDEEz <dryestboot@gmail.com>
2022-10-18 20:47:21 +0800

    Add project files.

---
+++ Parol/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace Parol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.IndexBox = new System.Windows.Forms.TextBox();
+            this.ColorBox = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(251, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(251, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 49);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(170, 226);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 49);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(89, 193);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 49);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(31, 129);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 49);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(89, 56);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 49);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // IndexBox
+            // 
+            this.IndexBox.Location = new System.Drawing.Point(156, 122);
+            this.IndexBox.Name = "IndexBox";
+            this.IndexBox.Size = new System.Drawing.Size(100, 20);
+            this.IndexBox.TabIndex = 8;
+            // 
+            // ColorBox
+            // 
+            this.ColorBox.Location = new System.Drawing.Point(156, 148);
+            this.ColorBox.Name = "ColorBox";
+            this.ColorBox.Size = new System.Drawing.Size(100, 20);
+            this.ColorBox.TabIndex = 9;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(170, 174);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Submit";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 299);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.ColorBox);
+            this.Controls.Add(this.IndexBox);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox IndexBox;
+        private System.Windows.Forms.TextBox ColorBox;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Parol/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parol
+{
+    public partial class Form1 : Form
+    {
+        Button[] buttons;
+        Color[] btnColors;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttons = new Button[]{button1,button2,button3,button4,button5,button6,button7,button8};
+            btnColors = new Color[buttons.Length];
+            for (int i = 0; i < btnColors.Length; i++)
+            {
+                btnColors[i] = Color.Transparent;
+            }
+            timer1.Start();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            //setButtonColor(IndexBox.Text)
+            try
+            {
+                int index = Convert.ToInt32(IndexBox.Text);
+                Color color = stringtoColor(ColorBox.Text);
+                setButtonColor(index -1, color);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Invalid: "+ err.Message);
+            }
+
+        }
+        Color stringtoColor(String str)
+        {
+            switch (str.ToLower())
+            {
+                case "red":return Color.Red;
+                case "blue":return Color.Blue;
+                case "green":return Color.Green;
+                case "yellow":return Color.Yellow;
+                case "violet":return Color.Violet;
+                case "brown":return Color.Brown;
+                case "black":return Color.Black;
+                default:return Color.Transparent;
+            }
+        }
+        void setButtonColor(int index,Color color)
+        {
+            buttons[index].BackColor = color;
+            btnColors[index] = color;
+        }
+        bool blink = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            clearAllBtnColor();
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if (blink == true)
+                {
+                    if (i % 2 == 0) buttons[i].BackColor = btnColors[i];
+                }
+                else
+                {
+                    if (i % 2 != 0) buttons[i].BackColor = btnColors[i];
+                }
+            }
+            blink = !blink;
+        }
+        void clearAllBtnColor()
+        {
+            foreach (Button button in buttons)
+            {
+                button.BackColor = Color.Transparent;
+            }
+        }
+    }
+}
